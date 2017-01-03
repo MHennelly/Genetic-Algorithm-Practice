c61c227d9dfaee04b11f3e3c2eb4a87793c73bea
MHennelly <michaelthehennelly@gmail.com>
2017-01-02 17:22:57 -0800

    Functional, as in it evolves towards the optimal solution

--- GA Practice 1/Project 1/GA1.py
+++ GA Practice 1/Project 1/GA1.py
@@ -1,5 +1,4 @@
 import random
-import math
 
 class Gene:
     def __init__(self, code):
@@ -11,43 +10,52 @@ class Gene:
 
     def rand(self, length):
         while(length > 0):
-            self.code += str(chr(random.randrange(0,255,1)))
+            self.code += str(chr(random.randrange(33,123,1)))
             length -= 1
 
     def calcCost(self, compareTo):
         total = 0
         for i in range(0,len(self.code)):
-            total += (ord(self.code[i]) - ord(compareTo[i]) * ord(self.code[i]) - ord(compareTo[i]))
+            total += ((ord(self.code[i]) - ord(compareTo[i])) * (ord(self.code[i]) - ord(compareTo[i])))
         self.cost = total
 
-    def mate(self, gene):
+    def mate_one(self, gene):
         pivot = round(len(self.code)/2,0) - 1
         child1 = self.code[0:int(pivot)] + gene.code[int(pivot):]
+        return Gene(child1)
+
+    def mate_two(self, gene):
+        pivot = round(len(self.code)/2,0) - 1
         child2 = gene.code[0:int(pivot)] + self.code[int(pivot):]
-        return [Gene(child1), Gene(child2)]
+        return Gene(child2)
 
     def mutate(self, chance):
-        if (random.uniform(0,1) > chance):
+        if chance > random.uniform(0,1):
             return
         else:
             s = list(self.code)
-            index = math.floor(random.uniform(0,1) * len(s))
+            mutation = ''
+            index = random.randint(0,len(self.code) - 1)
             upOrDown = random.uniform(0,1)
-
-            if (upOrDown > 0.5):
+            if upOrDown > 0.5:
                 s[index] = chr(ord(s[index]) + 1)
-                self.code = ''
-                self.code.join(s)
             else:
                 s[index] = chr(ord(s[index]) - 1)
-                self.code = ''
-                self.code.join(s)
+            for i in range (0,len(s)):
+                mutation += str(s[i])
+            self.code = mutation
+
+
+
+def sort_pop(self, member):
+    return member.cost
 
 class Population:
-    def __init__(self, goal, size):
+    def __init__(self, goal, size, generations):
         self.members = []
         self.goal = goal
         self.generationNumber = 0
+        self.generations = generations
         while (size > 0):
             gene = Gene('')
             gene.rand(len(self.goal))
@@ -55,29 +63,34 @@ class Population:
             size -= 1
 
     def generation(self):
+        while(self.generationNumber < self.generations):
+            self.generationNumber += 1
+            print('Generation: ' + str(self.generationNumber))
 
-
-
-
-        for i in range(0,10):
             for i in range(0,len(self.members)):
                 self.members[i].calcCost(self.goal)
-
-            for i in range(0, len(self.members)):
                 print (self.members[i].code)
 
-            for i in range(0,len(self.members) - 1,2):
-                children = self.members[0].mate(self.members[1])
-
+            self.members.sort(key =lambda gene: gene.cost)
 
             for i in range(0,len(self.members)):
+                print (self.members[i].cost)
+
+            child1 = self.members[0].mate_one(self.members[1])
+            child2 = self.members[0].mate_two(self.members[1])
+
+            for i in range(0,len(self.members),2):
+                self.members[i] = child1
+
+            for i in range(1,len(self.members),2):
+                self.members[i] = child2
+
+            for i in range(0, len(self.members)):
                 self.members[i].mutate(0.5)
                 self.members[i].calcCost(self.goal)
                 if (self.members[i].code == self.goal):
                     print(self.members[i])
                     return True
 
-            self.generationNumber += 1
-
-population = Population("Hello, world!",20)
+population = Population("Hello, world!",100, 10000)
 population.generation()
